00da0297ea824b250af2009d5df63ec091826896
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-03-14 12:28:01 +0530

    Observables basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'RxJSDemo';
+export class AppComponent implements OnInit {
+
+  public superheros: Observable<string> | undefined;
+  public superheroName: string | undefined;
+
+  ngOnInit(): void {
+
+    this.superheros = new Observable(function (observer) {
+      try {
+        observer.next("Ironman");
+        setInterval(() => {
+          observer.next("Antman");
+        }, 3000);
+        setInterval(() => {
+          observer.next("Spiderman");
+        }, 6000);
+      } catch (error) {
+        observer.error(error);
+      }
+    });
+
+    this.superheros.subscribe((data) => {
+      this.superheroName = data;
+    });
+
+  }
+
 }
